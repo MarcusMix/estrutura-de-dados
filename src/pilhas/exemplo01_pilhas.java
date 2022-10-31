0ba8457885b29fc6dbcb376cb6d01624ecaab60d
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-31 13:42:52 -0300

    feat: create example 01 of pilhas (stacks)

--- src/pilhas/exemplo01_pilhas.java
+++ src/pilhas/exemplo01_pilhas.java
@@ -1,10 +1,35 @@
 package pilhas;
 
+import java.util.Stack;
+
 public class exemplo01_pilhas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Stack<String> carros = new Stack<>();
+		
+		carros.push("Golf");
+		carros.push("Celta");
+		carros.push("Fusion");
+		carros.push("Fusca");
+		carros.push("Camaro");
+		
+//		for(String c:carros) {
+//			System.out.println(c);
+//		}
+		
+		
+		//retorna o topo da pilha
+		System.out.println("Topo da pilha: " + carros.peek());
+		
+		
+		//retorna o topo da pilha e retira ele da pilha
+		System.out.println("Topo da pilha" + carros.peek());
+		
+		//retorna booleann, se a pilha está vazia ou não
+		System.out.println("Pilha está vazia? " + (carros.empty()? "Sim!": "Não!"));
+		
+		//limpa a pilha
+		carros.clear();
 	}
 
 }
