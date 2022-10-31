f68cc45b78fc822532b5b659bb1f44c58567eb31
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-31 16:26:13 -0300

    feat: remove and add element for list and test it

--- src/lista/Programa.java
+++ src/lista/Programa.java
@@ -3,7 +3,6 @@ package lista;
 public class Programa {
 	public static void main(String[] args) {
 		ListaLigada lista = new ListaLigada();
-//		System.out.println(lista.getTamanho());
 		lista.adicionar("AC");
 		lista.adicionar("BA");
 		lista.adicionar("SC");
@@ -12,10 +11,16 @@ public class Programa {
 		System.out.println("Tamanho: " + lista.getTamanho());
 		System.out.println("Primeiro elemento: " + lista.getPrimeiro().getValor());
 		System.out.println("Ultimo elemento: " + lista.getUltimo().getValor());
-		System.out.println(lista.get(0).getValor());
-		System.out.println(lista.get(1).getValor());
-		System.out.println(lista.get(2).getValor());
-		System.out.println(lista.get(3).getValor());
+		
+
+		lista.remover("AC");
+		System.out.println("Removeu o estado AC");
+		lista.adicionar("SP");
+		System.out.println("Adicionou o estado SP");
+		
+		for(int i = 0; i < lista.getTamanho(); i++) {
+			System.out.println(lista.get(i).getValor());
+		}
 		
 	}
 }
