8bde578a28c64a87558cd293e3577adac7b06e6a
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-15 16:54:43 -0300

    feat: add prova1 and resolution

--- src/fila/exemplo01_fila.java
+++ src/fila/exemplo01_fila.java
@@ -1,5 +1,22 @@
 package fila;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class exemplo01_fila {
 
+	
+	public static void main(String[] args) {
+		
+		Queue<String> carros = new LinkedList<>();
+		
+		carros.add("hrv");
+		carros.add("ferrari");
+		carros.add("polo");
+		carros.add("camaro");
+		
+		System.out.println("Topo da fila: " + carros.poll()); // pega o primeiro e remove da lista
+		System.out.println(carros);
+		System.out.println(carros.isEmpty()? "Fila vazia" : "Fila com elementos");
+	}
 }
---
+++ src/prova1/prova1.java
@@ -0,0 +1,53 @@
+package prova1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class prova1 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		Scanner teclado = new Scanner(System.in);
+		
+		int fila[] = new int[20];
+		Queue<Integer> filaQueue = new LinkedList<>();
+		Stack<Integer> pilhaStack = new Stack<>();
+		
+		int maior = 0;
+		int menor = 99;
+		int soma = 0;
+		
+		for(int x = 0; x < fila.length; x++) {
+			int numeros = random.nextInt(26);
+			if(numeros < menor) {
+				menor = numeros;				
+			} 
+			if (numeros > maior) {
+				maior = numeros;
+			}
+			filaQueue.add(numeros);
+			pilhaStack.add(numeros);
+			soma += numeros;
+		}
+		
+		double media = soma / fila.length;
+		System.out.println("Fila pronta, aperte ENTER para continuar");
+		
+		while(true) {
+			String input  = teclado.nextLine();
+			if (!input.equals("")) {
+			} else {
+				System.out.println("Impressão da fila: " + filaQueue);	
+				System.out.println("Impressão da pilha: " + pilhaStack);
+				System.out.println("Maior número criado pelo Random: " + maior);
+				System.out.println("Menor número criado pelo Random: " + menor);
+				System.out.println("A média dos números criados é: " + media);
+			}
+		}
+			
+	}
+
+}
