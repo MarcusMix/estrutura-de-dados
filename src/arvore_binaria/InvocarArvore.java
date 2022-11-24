71e0a40991ff81ab283f1d4f7150e8a65d7ab72a
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-24 15:39:40 -0300

    feat: add remove Elements method

--- src/arvore_binaria/InvocarArvore.java
+++ src/arvore_binaria/InvocarArvore.java
@@ -12,13 +12,51 @@ public class InvocarArvore {
 		arvore.adicionar(18);
 		arvore.adicionar(13);
 		arvore.adicionar(20);
+		
 		System.out.println("\nEm-ordem");
 		arvore.emOrdem(arvore.getRaiz());
 		
-		System.out.println("\nPré-ordem");
-		arvore.preOrdem(arvore.getRaiz());
+		arvore.remover(20);
 		
-		System.out.println("\nPós-ordem");
-		arvore.posOrdem(arvore.getRaiz());
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(5);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(8);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(9);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(13);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(7);
+		arvore.remover(18);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		arvore.remover(10);
+		
+		System.out.println("\nEm-ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		
+		//System.out.println("\nPré-ordem");
+		//arvore.preOrdem(arvore.getRaiz());
+		
+		//System.out.println("\nPós-ordem");
+		//arvore.posOrdem(arvore.getRaiz());
 	}
 }
